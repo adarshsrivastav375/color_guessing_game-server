b7183208c2a1a013b4e1ef171074be91a15fc2d9
Adarsh srivastav <adarshsrivastav375@gmail.com>
2024-02-22 19:49:44 +0530

    made custom utilities for res and errror handling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 export default app;
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,12 @@
+const asyncHandler = (requestHandler) => async (req, res, next) => {
+  try {
+    await requestHandler(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+export { asyncHandler };
+
