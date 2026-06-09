6f5f2e286a77eb510aab15596080139520be7559
Adarsh Srivastav <adarsh@cvinfotech.com>
2026-06-10 04:42:38 +0530

    pm2 config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'color-guessing-game-server',
+      script: 'src/index.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      watch: false,
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      max_memory_restart: '500M',
+    },
+  ],
+};
